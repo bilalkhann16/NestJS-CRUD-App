4e7f4a7adf6a6dc3535fe08de174d77fd8ab5706
Bilal K <lalbikhan2014@gmail.com>
2022-07-25 01:38:32 +0500

    nested validation

--- src/customers/dtos/CreateCustomer.dto.ts
+++ src/customers/dtos/CreateCustomer.dto.ts
@@ -1,13 +1,20 @@
-import { IsEmail, IsNotEmpty, IsNumber } from "class-validator";
+import { Type } from "class-transformer";
+import { IsEmail, IsNotEmpty, IsNotEmptyObject, IsNumber, IsNumberString, ValidateNested } from "class-validator";
+import { CreateAddressDto } from "./CreateAddressDto";
 
 // the pattern how our using the Post request.
 export class CreateCustomerDto{
     @IsEmail()
     email: string;
 
-    @IsNumber()
+    @IsNumberString()
     id: number;
 
     @IsNotEmpty()
     name: string;
+
+    @ValidateNested()
+    @IsNotEmptyObject()
+    @Type(() => CreateAddressDto)
+    address: CreateAddressDto;
 }
